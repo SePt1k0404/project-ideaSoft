b179aa5647147ca1958535442481372c4a4f194a
SophiaTretiakova <122561236+SophiaTretiakova@users.noreply.github.com>
2023-08-03 18:45:37 +0300

    second commit

--- src/js/renderShoppingList.js
+++ src/js/renderShoppingList.js
@@ -1,1 +1,54 @@
+async function renderCards() {
+  const bookListEl = document.querySelector('#bookList');
+  const storedBooks = localStorage.getItem('books');
+  if (!storedBooks) {
+    console.log(error);
+    return;
+  }
+  const books = JSON.parse(storedBooks);
+  if (books.length === 0) {
+    const emptyListMessage = document.createElement('li');
+    emptyListMessage.textContent =
+      'This page is empty, add some books and proceed to order.';
+    bookListEl.appendChild(emptyListMessage);
+  }
+  const bookListHTML = books
+    .map(
+      book => `<li>
+              <div class="book-card">
+                 <button type="button" class="book-delete-btn" onclick="OnRemoveClick('${
+                   book._id
+                 }')">
+                    <svg class="book-delete-icon">
+                        <use href="/images/delete-book-icon.svg"></use>
+                    </svg>
+                </button>
+                <img src="${book.book_image}" alt="${
+        book.title
+      }" class="book-img">
+                <div class="book-text-content">
+                  <h3 class="book-title">${book.title}</h3>
+                  <p class="book-category">${book.list_name}</p>
+                  <p class="book-desc">${book.description}</p>
+                  <div class="book-copyright"> <p class="book-author">${
+                    book.author
+                  }</p>
+                  <div class="buy-links">
+                    ${book.buy_links
+                      .map(
+                        link => `
+                          <a href="${link.url}" target="_blank">${link.name}</a>
+                        `
+                      )
+                      .join('')}</div>
+                </div>
+                </div>
+              </div>
+              </li>
+            `
+    )
+    .join('');
+  bookListEl.innerHTML = bookListHTML;
+}
 
+export { renderCards };
--- src/shopping-list.js
+++ src/shopping-list.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
 // import './js/theme-toggle';
 // import './js/header';
 // import './js/render-support';
@@ -8,6 +10,7 @@
 // import './js/modal-team';
 // import './js/footer-toggle.js';
 // import './js/window-resize';
+import { renderCards } from './js/renderShoppingList';
 
 const booksData = [
   {
@@ -16,13 +19,21 @@ const booksData = [
     title: 'WISH',
     author: "Barbara O'Connor",
     description:
-      'Це дуже цікава книга Це дуже цікава книга Це дуже цікава книга Це дуже цікава книга про...',
+      'Це дуже цікава книга Це дуже цікава книга Це дуже цікава книга Це дуже цікава книга',
     book_image:
       'https://storage.googleapis.com/du-prd/books/images/9781250144058.jpg',
     buy_links: [
       {
         name: 'Amazon',
         url: 'https://www.amazon.com/Wish-Barbara-OConnor/dp/1250144051?tag=NYTBSREV-20',
+      },
+      {
+        name: 'Apple Books',
+        url: 'https://goto.applebooks.apple/9781250144058?at=10lIEQ',
+      },
+      {
+        name: 'Barnes and Noble',
+        url: 'https://www.anrdoezrs.net/click-7990613-11819508?url=https%3A%2F%2Fwww.barnesandnoble.com%2Fw%2F%3Fean%3D9781250144058',
       },
     ],
   },
@@ -46,52 +57,10 @@ const booksData = [
 
 localStorage.setItem('books', JSON.stringify(booksData));
 
-document.addEventListener('DOMContentLoaded', renderCards);
-
-function renderCards() {
-  const bookListElement = document.getElementById('bookList');
-  const storedBooks = localStorage.getItem('books');
-
-  if (storedBooks) {
-    const books = JSON.parse(storedBooks);
-
-    if (books.length > 0) {
-      const bookListHTML = books
-        .map(
-          book => `
-                        <div class="book-card">
-                            <img src="${book.book_image}" alt="${
-            book.title
-          }" class="book-img">
-                            <div class="book-text-content">
-                                <h3 class="book-title">${book.title}</h3>
-                                <p class="book-category">${book.list_name}</p>
-                                <p class="book-desc">${book.description}</p>
-                                <p class="book-author">${book.author}</p>
-                                <div class="buy-links">
-                                    ${book.buy_links
-                                      .map(
-                                        link => `
-                                        <a href="${link.url}" target="_blank">${link.name}</a>
-                                    `
-                                      )
-                                      .join('')}
-                                </div>
-                            </div>
-                        </div>
-                    `
-        )
-        .join('');
-
-      bookListElement.innerHTML = bookListHTML;
-    } else {
-      const emptyListMessage = document.createElement('li');
-      emptyListMessage.textContent = 'Список книг порожній.';
-      bookListElement.appendChild(emptyListMessage);
-    }
-  } else {
-    const emptyListMessage = document.createElement('li');
-    emptyListMessage.textContent = 'Список книг порожній.';
-    bookListElement.appendChild(emptyListMessage);
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    await renderCards();
+  } catch (error) {
+    console.error('Error rendering cards:', error);
   }
-}
+});
