860c3dd06cb6e926dbb4d093a009e226aded662a
Yuriy <yuriylviv2006@gmail.com>
2023-08-04 00:09:48 +0300

    add local storage and refs

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export default {
+    //   books-content-by-category.js, render-book-info.js
+    contentWrapper: document.querySelector('.js-content-wrapper'),
+    backdropLoader: document.querySelector('.loader-backdrop'),
+ //   onRemoveClick.js
+  shoppingListWrapper: document.querySelector('.shopping-list-wrap'),
