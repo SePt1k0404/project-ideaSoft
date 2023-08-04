be4e8217092955e3e9a39549d2e0d45131783072
SePt1k0404 <99293859+SePt1k0404@users.noreply.github.com>
2023-08-04 20:15:50 +0000

    Deploying to gh-pages from @ SePt1k0404/project-ideaSoft@e07e07ecbc721c913724d5e88a2bdc47bdc9525f 🚀

---
+++ index.884557f2.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".header__theme-switcher"),t=document.querySelector(".header__burger"),c=(document.querySelector(".header__nav"),document.querySelector(".header__modal")),d=document.querySelector(".header__modal-close");e.addEventListener("click",(function(){document.body.classList.toggle("dark-theme")})),t.addEventListener("click",(function(){c.style.display="flex"})),d.addEventListener("click",(function(){c.style.display="none"}));
+//# sourceMappingURL=index.884557f2.js.map
