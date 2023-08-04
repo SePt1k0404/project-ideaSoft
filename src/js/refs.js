23950705363db207b8fd15f04a2fb3eae710c0fc
SePt1k0404 <99293859+SePt1k0404@users.noreply.github.com>
2023-08-04 19:48:36 +0300

    fix

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,5 +2,6 @@ export default {
     //   books-content-by-category.js, render-book-info.js
     contentWrapper: document.querySelector('.js-content-wrapper'),
     backdropLoader: document.querySelector('.loader-backdrop'),
- //   onRemoveClick.js
-  shoppingListWrapper: document.querySelector('.shopping-list-wrap'),
+    //   onRemoveClick.js
+    shoppingListWrapper: document.querySelector('.shopping-list-wrap'),
+}
