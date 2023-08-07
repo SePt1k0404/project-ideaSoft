71f15660b91ed32579f8ba64b9955e3185d300c6
SePt1k0404 <99293859+SePt1k0404@users.noreply.github.com>
2023-08-07 17:52:12 +0300

    some header css

--- src/js/header.js
+++ src/js/header.js
@@ -5,27 +5,32 @@ const modal = document.querySelector('.header__modal');
 const modalClose = document.querySelector('.header__modal-close');
 const switcherBgLigth = document.querySelector('.header__theme-light-switch');
 const switcherBgDark = document.querySelector('.header__theme-dark-switch');
-const switcherThemeButton = document.querySelector('.header__theme-round-button');
+const switcherThemeButton = document.querySelector(
+  '.header__theme-round-button'
+);
+const bodyScrollLock = require('body-scroll-lock');
+const disableBodyScroll = bodyScrollLock.disableBodyScroll;
+const enableBodyScroll = bodyScrollLock.enableBodyScroll;
 
 // Function to toggle the dark theme
 function toggleTheme() {
   document.body.classList.toggle('dark-theme');
   switcherBgLigth.classList.toggle('ligth-switcher');
   switcherBgDark.classList.toggle('dark-switcher');
-  switcherThemeButton.classList.toggle('bias-btn')
-  }
+  switcherThemeButton.classList.toggle('bias-btn');
+}
 
 // Function to open the modal
 function openModal() {
+  disableBodyScroll(document.body);
   modal.style.display = 'flex';
   burgerMenu.style.display = 'none';
   modalClose.style.display = 'block';
 }
 
-
-
 // Function to close the modal
 function closeModal() {
+  enableBodyScroll(document.body);
   modal.style.display = 'none';
   burgerMenu.style.display = 'block';
   modalClose.style.display = 'none';
@@ -36,16 +41,15 @@ themeSwitcher.addEventListener('click', toggleTheme);
 burgerMenu.addEventListener('click', openModal);
 modalClose.addEventListener('click', closeModal);
 
-  // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    modal.style.display = 'none';
-    burgerMenu.style.display = 'none';
-    modalClose.style.display = 'none';
-  });
+// Close the mobile menu on wider screens if the device orientation changes
+window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  if (!e.matches) return;
+  modal.style.display = 'none';
+  burgerMenu.style.display = 'none';
+  modalClose.style.display = 'none';
+});
 
-  window.matchMedia('(max-width: 767px)').addEventListener('change', e => {
-    if (!e.matches) return;
-        burgerMenu.style.display = 'block';
-     });
-
+window.matchMedia('(max-width: 767px)').addEventListener('change', e => {
+  if (!e.matches) return;
+  burgerMenu.style.display = 'block';
+});
