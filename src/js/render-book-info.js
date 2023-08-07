f76d4c5c1aff35bc28316f72c012de7fe2a38b4a
SePt1k0404 <99293859+SePt1k0404@users.noreply.github.com>
2023-08-07 10:57:11 +0300

    add modals to home page

--- src/js/render-book-info.js
+++ src/js/render-book-info.js
@@ -1,11 +1,14 @@
 import { markupCardBookInfo } from './markup-books-info';
 // import getBookAPI from './getBookAPI';
-import ApiClientAxios from './getBookAPI';
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
 import { onClickBtn } from './onClickBtn';
 import { searchBook } from './addToLocalStorage';
 import { refs } from './refs';
+
+const bodyScrollLock = require('body-scroll-lock');
+const disableBodyScroll = bodyScrollLock.disableBodyScroll;
+const enableBodyScroll = bodyScrollLock.enableBodyScroll;
 
 refs.contentWrapper.addEventListener('click', onBookInfoClick);
 async function onBookInfoClick(evt) {
@@ -52,48 +55,3 @@ async function onBookInfoClick(evt) {
     console.log(error);
   }
 }
-
-// async function onBookInfoClick(evt) {
-//   try {
-//     evt.preventDefault();
-//     const cardLink = evt.target.closest('.js-card-link');
-//     if (!cardLink) {
-//       return;
-//     }
-
-//     const bookId = cardLink.dataset.id;
-//     const data = await getBookAPI('bookId', bookId);
-//     const infoMarkup = markupCardBookInfo(data, searchBook(data));
-
-//     const instance = basicLightbox.create(infoMarkup, {
-//       onShow: () => {
-//         disableBodyScroll(document.body);
-//         window.addEventListener('keydown', onEscButtonClick);
-//       },
-//       onClose: () => {
-//         enableBodyScroll(document.body);
-//         window.removeEventListener('keydown', onEscButtonClick);
-//       },
-//     });
-
-//     instance.show();
-//     const modalInfoClose = document.querySelector('[data-modal-close]');
-//     modalInfoClose.addEventListener('click', onBookInfoModalCloses);
-
-//     function onBookInfoModalCloses() {
-//       instance.close();
-//     }
-
-//     const actionBtn = document.querySelector('.btn-book-info');
-
-//     actionBtn.addEventListener('click', onClickBtn);
-
-//     function onEscButtonClick(evt) {
-//       if (evt.code === 'Escape') {
-//         instance.close();
-//       }
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
