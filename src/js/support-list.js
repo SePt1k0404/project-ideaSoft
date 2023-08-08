74870d6495ded775035c9d419729a2c6e9ff4ef3
SePt1k0404 <99293859+SePt1k0404@users.noreply.github.com>
2023-08-08 12:35:57 +0300

    support mobile fix

--- src/js/support-list.js
+++ src/js/support-list.js
@@ -3,36 +3,43 @@ import { refs } from './refs';
 window.addEventListener('load', onPageLoad);
 refs.supportBtn.addEventListener('click', onClickSupportBtn);
 let supportLi = [];
+let width = null;
 
 function onClickSupportBtn(evt) {
-  for (let i = 0; i < supportLi.length; i += 1) {
-    supportLi[i].classList.toggle('visually-hidden');
+  if (width >= 768) {
+    for (let i = 0; i < supportLi.length; i += 1) {
+      supportLi[i].classList.toggle('visually-hidden');
+    }
+    supportLi[5].classList.toggle('last-of-mobile');
+    supportLi[8].classList.toggle('last-of-mobile');
+    refs.supportBtn.classList.toggle('support-button-rotate');
+  } else {
+    for (let i = 0; i < supportLi.length; i += 1) {
+      supportLi[i].classList.toggle('visually-hidden');
+    }
+    supportLi[3].classList.toggle('last-of-mobile');
+    supportLi[8].classList.toggle('last-of-mobile');
+    refs.supportBtn.classList.toggle('support-button-rotate');
+    refs.supportContainer.classList.toggle('support-container-hight');
   }
-  supportLi[5].classList.toggle('last-of-mobile');
-  supportLi[8].classList.toggle('last-of-mobile');
-  refs.supportBtn.classList.toggle('support-button-rotate');
 }
 
 function onPageLoad() {
-  const width = window.innerWidth;
+  width = window.innerWidth;
+  supportLi = refs.supportList.children;
   const currentPage = window.location.pathname;
   const indexPages = [
     '/shopping-list.html',
     '/project-ideaSoft/shopping-list.html',
   ];
-  console.log(currentPage);
   if (indexPages.includes(currentPage) && width >= 1440) {
-    console.log('!indexPages.includes(currentPage) && width >= 1440');
     refs.supportContainer.classList.remove('visually-hidden');
   } else if (indexPages.includes(currentPage) && width < 1440) {
-    console.log('!indexPages.includes(currentPage) && width < 1440');
     refs.supportContainer.classList.add('visually-hidden');
   } else {
-    console.log('else');
     refs.supportContainer.classList.remove('visually-hidden');
   }
   if (width >= 768) {
-    supportLi = refs.supportList.children;
     supportLi[3].classList.remove('last-of-mobile');
     supportLi[4].classList.remove('visually-hidden');
     supportLi[5].classList.remove('visually-hidden');
