947a5c19cec0b6a55932e7533fa4a79abe109e6b
Zelenro <125198729+Zelenro@users.noreply.github.com>
2023-08-08 21:55:55 +0300

    headerForTestOnWorking

--- src/js/header.js
+++ src/js/header.js
@@ -5,12 +5,27 @@ const disableBodyScroll = bodyScrollLock.disableBodyScroll;
 const enableBodyScroll = bodyScrollLock.enableBodyScroll;
 
 // Function to toggle the dark theme
+// function toggleTheme() {
+//   document.body.classList.toggle('dark-theme');
+//   refs.switcherBgLight.classList.toggle('light-switcher');
+//   refs.switcherBgDark.classList.toggle('dark-switcher');
+//   refs.switcherThemeButton.classList.toggle('bias-btn');
+//   if (refs.switcherBgLight.classList.contains('light-switcher')) {
+//     localStorage.setItem(THEME_KEY, 'dark');
+//     refs.switcherHtml.dataset.theme = 'dark';
+//   } else {
+//     localStorage.setItem(THEME_KEY, 'light');
+//     refs.switcherHtml.dataset.theme = 'light';
+//   }
+// }
+
 function toggleTheme() {
-  document.body.classList.toggle('dark-theme');
+  const isDarkTheme = document.body.classList.toggle('dark-theme');
   refs.switcherBgLight.classList.toggle('light-switcher');
   refs.switcherBgDark.classList.toggle('dark-switcher');
   refs.switcherThemeButton.classList.toggle('bias-btn');
-  if (refs.switcherBgLight.classList.contains('light-switcher')) {
+
+  if (isDarkTheme) {
     localStorage.setItem(THEME_KEY, 'dark');
     refs.switcherHtml.dataset.theme = 'dark';
   } else {
@@ -21,7 +36,8 @@ function toggleTheme() {
 
 // Function to open the modal
 function openModal() {
-  disableBodyScroll(document.body);
+  // disableBodyScroll(document.body);
+  document.body.classList.add('no-scroll');
   refs.modal.style.display = 'flex';
   refs.burgerMenu.style.display = 'none';
   refs.modalClose.style.display = 'block';
@@ -29,7 +45,8 @@ function openModal() {
 
 // Function to close the modal
 function closeModal() {
-  enableBodyScroll(document.body);
+  // enableBodyScroll(document.body);
+  document.body.classList.remove('no-scroll');
   refs.modal.style.display = 'none';
   refs.burgerMenu.style.display = 'block';
   refs.modalClose.style.display = 'none';
